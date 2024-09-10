70cb62361a0952dbefa47a9b9cb1e2b8f425130c
Pedro Schinke <pedro.schinke@outlook.com>
2024-09-10 14:29:25 -0300

    improved filter

--- app/filter.js
+++ app/filter.js
@@ -1,13 +1,35 @@
-import { showBooks } from "./forEach.js";
-import { books } from "./main.js";
-
 const filterButtons = document.querySelectorAll('.btn');
 
-filterButtons.forEach(btn => btn.addEventListener('click', frontEndFilter));
+filterButtons.forEach(btn => btn.addEventListener('click', filterBooks));
 
-function frontEndFilter() {
+function filterBooks() {
     const btnElement = document.getElementById(this.id);
     const category = btnElement.value;
-    let filteredBooks = books.filter(book => book.categoria == category);
+
+    let filteredBooks = category == 'available' ? 
+        filterByAvailability() : 
+        filterByCategory(category);
+
     showBooks(filteredBooks);
+
+    if (category == 'available') {
+        const totalPrice = getAvailableBooksValue(filteredBooks);
+        showTotalPrice(totalPrice);
+    }
+}
+
+function filterByAvailability() {
+    return books.filter(book => book.quantidade > 0);
+}
+
+function filterByCategory(category) {
+    return books.filter(book => book.categoria == category);
+}
+
+function showTotalPrice(totalPrice) {
+    availableBooksTotalPrice.innerHTML = `
+        <div class="available_books">
+            <p>All books available for $<span id="value">${totalPrice}</span></p>
+        </div>
+    `;
 }
--- app/forEach.js
+++ app/forEach.js
@@ -1,16 +1,21 @@
-const booksContainer = document.getElementById('books');
+const availableBooksTotalPrice = document.getElementById('available_books_total_value');
 
-export function showBooks(booksList) {
+function showBooks(booksList) {
+    availableBooksTotalPrice.innerHTML = '';
+
     booksContainer.innerHTML = '';
+
     booksList.forEach(book => {
+        let isAvailable = book.quantidade > 0 ? 'book__images' : 'book_images unavailable';
+
         booksContainer.innerHTML += `
             <div class="book">
-                <img class="book__images" src="${book.imagem}" alt="${book.titulo} cover" />
+                <img class="${isAvailable}" src="${book.imagem}" alt="${book.titulo} cover" />
                 <h2 class="book__title">
                     ${book.titulo}
                 </h2>
                 <p class="book__description">${book.autor}</p>
-                <p class="book__price" id="price">R$${book.preco.toFixed(2)}</p>
+                <p class="book__price" id="price">$${book.preco.toFixed(2)}</p>
                 <div class="tags">
                     <span class="tag">${book.categoria}</span>
                 </div>
